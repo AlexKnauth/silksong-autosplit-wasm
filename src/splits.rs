e6dc717be1bc1e181b0ba2a79dbc87caa43d296c
AlexKnauth <alexander@knauth.org>
2025-09-26 14:58:13 -0400

    Restore original StartNewGame

--- src/splits.rs
+++ src/splits.rs
@@ -22,6 +22,11 @@ pub enum Split {
     /// Never splits. Use this when you need to manually split
     #[default]
     ManualSplit,
+    /// Start New Game (Start)
+    ///
+    /// Splits when starting a new save file
+    /// from the opening cutscenes into Moss Grotto
+    StartNewGame,
     /// Act 1 Start (Start)
     ///
     /// Splits when starting Act 1,
@@ -676,6 +681,9 @@ pub fn transition_splits(
 ) -> SplitterAction {
     match split {
         // region: Start, End, and Menu
+        Split::StartNewGame => {
+            should_split(OPENING_SCENES.contains(&scenes.old) && scenes.current == "Tut_01")
+        }
         Split::EndingSplit => should_split(scenes.current.starts_with("Cinematic_Ending")),
         Split::EndingA => should_split(scenes.current == "Cinematic_Ending_A"),
         Split::AnyTransition => should_split(true),
